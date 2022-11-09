d98fa6c93746aae829c00d0d57900eed4d3d454b
Unesio-Severino <unesio.private@gmail.com>
2022-11-10 00:56:58 +0200

    Finish Design.

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,17 +27,19 @@ export default function Home() {
 
       {/* PROFILE */}
 
-      <main className="bg-white px-10 md:px-20 lg:px-40">
+      <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
         <section className="min-h-screen">
-          <nav className="py-10 mb-12 flex justify-between">
+          <nav className="py-10 mb-12 flex justify-between dark:text-white">
             <h1 className="text-xl font-burtons">Unésio Mucutueliua</h1>
             <ul className="flex items-center">
               <li>
-                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className="cursor-pointer text-2xl" />
               </li>
               <li>
                 <a
-                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
                   href="#"
                 >
                   Resumo
@@ -46,53 +48,52 @@ export default function Home() {
             </ul>
           </nav>
 
-          <div className="text-center p-10">
-            <h2 className="text-6xl py-2 text-teal-600 font-medium md:text-6xl">
+          <div className="text-center p-10 py-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
               Unésio Mucutueliua
             </h2>
-            <h3 className="text-2xl py-2 md:text-3xl">
+            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
               Desenvolvedor web & Desenvolvedor de aplicações.
             </h3>
-            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto">
+            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
               Sou Desenvolvedor web, desenvolvedor de aplicações, jovem criativo
               e inovador com muita paixão pela tecnologia, desenho. Trabalho com
               desenvolvimento de softwares deste 2020. Entre em contacto comigo
               pelos serviços abaixo.
             </p>
           </div>
-          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
             <AiFillGithub />
             <AiFillInstagram />
             <AiFillLinkedin />
           </div>
 
-          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
-            <Image src={deved} layout="fill" objectFit="cover" alt={""} />
+          <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+            <Image src={deved} layout="fill" objectFit="cover" />
           </div>
         </section>
 
         {/* SERVICES */}
         <section>
           <div>
-            <h3 className="text-3xl py-1">Serviços que ofereço</h3>
-            <p className="text-md py-2 leading-8 text-gray-80">
+            <h3 className="text-3xl py-1 dark:text-white">Serviços que ofereço</h3>
+             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Desde o inicio da minha jornada como programador web, eu ja
               trabalhei como desenvolvedor freelance e suporte remoto
               <span className="text-teal-500"> na Inforem,Lda, </span>e
-              colaborei em pequenos{" "}
-              <span className="text-teal-500">projetos. </span>
+              colaborei em pequenos <span className="text-teal-500">projetos. </span>
               Mas a maioria deles sao projetos de ideias próprias.
             </p>
-            <p className="text-md py-2 leading-8 text-gray-80">
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Também ofereço serviços de analise, diagnostico e reparação de
               equipamentos computadores.
             </p>
           </div>
 
           <div className="lg:flex gap-10">
-            <div className="text-center shadow-lg p-10 rounded-xl my-10">
-              <Image src={design} width={120} height={120} />
-              <h3 className="text-lg font-medium pt-8 pb-2">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
+              <Image src={design} width={100} height={100} />
+              <h3 className="text-lg font-medium pt-8 pb-2  ">
                 Designs Responsivos
               </h3>
               <p className="py-2">
@@ -102,12 +103,13 @@ export default function Home() {
               <p className="text-gray-800 py-1">Photoshop</p>
               <p className="text-gray-800 py-1">Illustrator</p>
               <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Indesign</p>
             </div>
 
-            <div className="text-center shadow-lg p-10 rounded-xl my-10">
-              <Image src={code} width={120} height={120} />
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+              <Image src={code} width={100} height={100} />
               <h3 className="text-lg font-medium pt-8 pb-2">
-                Desenvolvimento Web e Aplicações
+                Desenvolvimento Web e Aplicações dos seus sonhos
               </h3>
               <p className="py-2">
                 Criando soluções, e desenvolvendo aplicações para diversos
@@ -117,10 +119,11 @@ export default function Home() {
               <p className="text-gray-800 py-1">Photoshop</p>
               <p className="text-gray-800 py-1">Illustrator</p>
               <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Indesign</p>
             </div>
 
-            <div className="text-center shadow-lg p-10 rounded-xl my-10">
-              <Image src={consulting} width={120} height={120} />
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+              <Image src={consulting} width={100} height={100} />
               <h3 className="text-lg font-medium pt-8 pb-2">Consultoria</h3>
               <p className="py-2">
                 Criando Belissimos designs que deixam tuas paginas vivas.
@@ -129,13 +132,15 @@ export default function Home() {
               <p className="text-gray-800 py-1">Photoshop</p>
               <p className="text-gray-800 py-1">Illustrator</p>
               <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Indesign</p>
             </div>
           </div>
-        </section>
-        <section>
+          </section>
+
+        <section className="py-10">
           <div>
-            <h3 className="text-3xl py-1">Meu Portfolio</h3>
-            <p className="text-md py-2 leading-8 text-gray-80">
+            <h3 className="text-3xl py-1 dark:text-white ">Meu Portofolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Desde o inicio da minha jornada como programador web, eu ja
               trabalhei como desenvolvedor freelance e suporte remoto
               <span className="text-teal-500"> na Inforem,Lda, </span>e
@@ -143,22 +148,22 @@ export default function Home() {
               <span className="text-teal-500">projetos. </span>
               Mas a maioria deles sao projetos de ideias próprias.
             </p>
-            <p className="text-md py-2 leading-8 text-gray-80">
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Também ofereço serviços de analise, diagnostico e reparação de
               equipamentos computadores.
             </p>
           </div>
-          <div className="flex flex-col gap-11 py-12 lg:flex-row lg:flex-wrap">
+          <div className="flex flex-col gap-11 py-11 lg:flex-row lg:flex-wrap">
             <div className="basis-1/3 flex-1">
               <Image
-                src={web1}
                 className="rounded-lg object-cover"
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
+                src={web1}
               />
             </div>
-            <div className="basis-1/3 flex-1">
+            <div className="basis-1/3 flex-1 ">
               <Image
                 src={web2}
                 className="rounded-lg object-cover"
@@ -167,7 +172,7 @@ export default function Home() {
                 layout="responsive"
               />
             </div>
-            <div className="basis-1/3 flex-1">
+            <div className="basis-1/3 flex-1 ">
               <Image
                 src={web3}
                 className="rounded-lg object-cover"
@@ -185,7 +190,7 @@ export default function Home() {
                 layout="responsive"
               />
             </div>
-           <div className="basis-1/3 flex-1">
+            <div className="basis-1/3 flex-1">
               <Image
                 src={web5}
                 className="rounded-lg object-cover"
